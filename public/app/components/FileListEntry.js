ab2119857d9448c8c2690d1bc63a9a6eebb4209d
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2015-07-06 19:38:57 +0200

    Rename icon on hover implemented

--- public/app/components/FileListEntry.js
+++ public/app/components/FileListEntry.js
@@ -1,6 +1,7 @@
 /* jshint esnext: true */
 
 import React from 'react';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
 class FileListEntry extends React.Component {
 
@@ -11,7 +12,8 @@ class FileListEntry extends React.Component {
 
         this.state = {
             file: props.file,
-            isChecked
+            isChecked,
+            isHovered: false
         };
     }
 
@@ -27,7 +29,7 @@ class FileListEntry extends React.Component {
 
     }
 
-    formatFilesize(size) {
+    _formatFilesize(size) {
         if(typeof size === 'undefined') return '';
 
         if(size === 0) return '0 B';
@@ -36,21 +38,29 @@ class FileListEntry extends React.Component {
         return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
     }
 
+    _toggleMouseState(state) {
+        this.setState({
+            isHovered: state
+        });
+    }
+
     render() {
 
         var file = this.state.file;
+
+        var renameButton = this.state.isHovered ? <Glyphicon glyph="pencil" /> : '';
 
         var href = (file.type === 'dir') ? '#/dir/' : '#/file/';
         href += file.path;
 
         var imgsrc = 'images/icon-' + file.type + '.png';
         return (
-            <tr onClick={this.onClick}>
+            <tr onClick={this.onClick} onMouseOver={this._toggleMouseState.bind(this, true)} onMouseLeave={this._toggleMouseState.bind(this, false)}>
                 <td><input onChange={this._onSelect.bind(this, file)} type="checkbox" checked={this.state.isChecked}/></td>
                 <td><img src={imgsrc} alt={file.type}/></td>
-                <td><a href={href}>{file.basename}</a></td>
+                <td><a href={href}>{file.basename}</a> {renameButton}</td>
                 <td className="text-right">A</td>
-                <td className="text-right">{this.formatFilesize(file.size)}</td>
+                <td className="text-right">{this._formatFilesize(file.size)}</td>
                 <td className="text-right">A</td>
                 <td className="text-right">X Y</td>
             </tr>
