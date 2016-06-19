c9b76f6c250aea7a5e30ef3201e079701c41b1e3
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2016-06-19 18:37:29 +0200

    Added directory download

--- app/Transfer/Download/DownloadTransfer.php
+++ app/Transfer/Download/DownloadTransfer.php
@@ -29,23 +29,36 @@ class DownloadTransfer
 
         $this->path = $path;
         $this->fs   = $fs;
+
         $this->createTempDir();
-        // Download all files into this directory
+        $this->fetchRemoteContents();
 
-        foreach ($this->path as $location) {
-            $directory = '/' . dirname($location);
-            $name      = str_replace($directory, '', $location);
-
-            $this->scanDirectory($directory, $name);
-        }
-
-        // $this->fs->get();
         // Create zip
         // Delete temporary directory
         // Send zip to user
         // Delete zip
     }
 
+    /**
+     * Download all remote contents to the local
+     * filesystem.
+     */
+    protected function fetchRemoteContents()
+    {
+        foreach ($this->path as $location) {
+            $directory = $this->makeAbsolute($location);
+            $location = trim($location, '/');
+
+            $this->downloadDirectory($directory, $location);
+        }
+    }
+
+    /**
+     * Create a local temp directory to store the downloaded
+     * files in.
+     *
+     * @return $this
+     */
     protected function createTempDir()
     {
         $this->temp = 'downloads/' . uniqid(session_id(), true);
@@ -56,27 +69,63 @@ class DownloadTransfer
     }
 
     /**
+     * Download a remote directory.
+     *
      * @param $directory
      * @param $filter
      */
-    protected function scanDirectory($directory, $filter = null)
+    protected function downloadDirectory($directory, $filter = null)
     {
+        // Make sure it's absolute to the current server root
+        $directory = '/' . $directory;
+
         foreach ($this->dirRepo->listing($directory) as $entry) {
-            if ($filter && $entry['basename'] !== $filter) {
+            // Skip this entry if a filter is set and the file does not match it
+            if ($filter && $entry['path'] !== $filter) {
                 continue;
             }
 
             if ($entry['type'] === 'file') {
-                $this->fs->drive('local')->put(
-                    $this->temp . '/' . $entry['path'] . '/' . $entry['basename'],
-                    $this->fs->read($entry['path'])
-                );
+                $this->downloadFile($entry);
             }
 
             if ($entry['type'] === 'dir') {
-                $this->fs->makeDirectory($this->temp . '/' . $entry['path']);
-                $this->scanDirectory('/' . $entry['path']);
+                $this->createLocalDirectory($entry['path']);
+                $this->downloadDirectory($entry['path']);
             }
         }
+    }
+
+    /**
+     * Make a path absolute.
+     *
+     * @param $location
+     *
+     * @return string
+     */
+    protected function makeAbsolute($location)
+    {
+        return dirname(trim($location, '/'));
+    }
+
+    /**
+     * Download a remote file to the local temp dir.
+     *
+     * @param $entry
+     */
+    protected function downloadFile($entry)
+    {
+        $this->fs->drive('local')->put(
+            $this->temp . '/' . $entry['path'],
+            $this->fs->read($entry['path'])
+        );
+    }
+
+    /**
+     * @param $path
+     */
+    protected function createLocalDirectory($path)
+    {
+        $this->fs->drive('local')->makeDirectory($this->temp . '/' . $path);
     }
 }
