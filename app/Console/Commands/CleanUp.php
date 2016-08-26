2668515bdaf3847814cba3797e4a06aef805e456
Tobias Kündig <tobias@offline.swiss>
2016-08-26 16:23:47 +0200

    Added cleanup command

---
+++ app/Console/Commands/CleanUp.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\FilesystemManager;
+
+class CleanUp extends Command
+{
+    /**
+     * @var int
+     */
+    const FIFTEEN_MINUTES = 15 * 60;
+
+    /**
+     * Never delete these files.
+     * @var array
+     */
+    private $ignore = ['.gitignore'];
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'onlineftp:cleanup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cleans up temporary files';
+
+    /**
+     * @var FilesystemManager
+     */
+    private $fs;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param FilesystemManager $fs
+     */
+    public function __construct(FilesystemManager $fs)
+    {
+        parent::__construct();
+        $this->fs = $fs;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach ($this->fs->disk('local')->files('downloads') as $file) {
+            if ($this->ignore($file) || ! $this->isOld($file)) {
+                continue;
+            }
+
+            $this->fs->disk('local')->delete($file);
+        }
+    }
+
+    /**
+     * Checks if a file should be ignored.
+     *
+     * @param $file
+     *
+     * @return bool
+     */
+    private function ignore($file)
+    {
+        $parts = explode('/', $file);
+
+        return in_array(array_pop($parts), $this->ignore);
+    }
+
+    /**
+     * Checks if a file should be deleted.
+     *
+     * @param $file
+     *
+     * @return bool
+     */
+    protected function isOld($file)
+    {
+        $modified = $this->fs->disk('local')->lastModified($file);
+
+        return $modified < (time() - $this::FIFTEEN_MINUTES);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CleanUp;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,18 +14,19 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Inspire::class,
+        CleanUp::class,
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->command('onlineftp:cleanup')
+                 ->everyTenMinutes();
     }
 }
