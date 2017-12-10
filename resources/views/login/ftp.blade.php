ee26beba133138bdfb2e10f98d23ec269ecd0192
Tobias Kündig <tobias@offline.swiss>
2017-12-10 14:11:46 +0100

    Pre-fill login form with get parameters

--- resources/views/login/ftp.blade.php
+++ resources/views/login/ftp.blade.php
@@ -1,24 +1,27 @@
 @include('helpers/input', [
     'id' => 'host',
     'label' => 'Host',
+    'value' => request()->get('host') ?? old('host'),
     'type' => 'text',
     'autofocus' => true
 ])
 @include('helpers/input', [
     'id' => 'username',
     'label' => 'Username',
+    'value' => request()->get('username') ?? old('username'),
     'type' => 'text'
 ])
 @include('helpers/input', [
     'id' => 'password',
     'label' => 'Password',
+    'value' => request()->get('password') ?? old('password'),
     'type' => 'password'
 ])
 @include('helpers/input', [
     'id' => 'port',
     'label' => 'Port',
     'type' => 'number',
-    'value' => 21
+    'value' => request()->get('port') ?? old('port') ?? 21,
 ])
 <div class="form-group">
     <button type="submit" class="btn btn-primary" id="btn-login">Login</button>
